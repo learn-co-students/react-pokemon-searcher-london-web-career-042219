bb4db3fb1600e01e04270183ac4e3dfaebb99d22
Pat <patrick.s.santucci@gmail.com>
2019-07-02 16:09:56 +0100

    Done.

--- src/components/PokemonCard.js
+++ src/components/PokemonCard.js
@@ -1,27 +1,55 @@
-import React from 'react'
-import { Card } from 'semantic-ui-react'
+import React from "react";
+import { Card } from "semantic-ui-react";
 
 class PokemonCard extends React.Component {
-  render() {
-    return (
-      <Card>
-        <div>
+  state = { front: true };
+
+  flip = () => this.setState({ front: !this.state.front });
+
+  renderCardSide = () => {
+    const pokemon = this.props.pokemon;
+    const hp = pokemon.stats.find(stat => stat.name === "hp").value;
+
+    if (this.state.front) {
+      return (
+        <div onClick={this.flip}>
           <div className="image">
-            <img alt="oh no!" />
+            <img src={pokemon.sprites.front} alt={pokemon.name} />
           </div>
           <div className="content">
-            <div className="header">POKEMON NAME HERE</div>
+            <div className="header">{pokemon.name}</div>
           </div>
           <div className="extra content">
             <span>
               <i className="icon heartbeat red" />
-              POKEMON HP HERE hp
+              {hp}
             </span>
           </div>
         </div>
-      </Card>
-    )
+      );
+    } else {
+      return (
+        <div onClick={this.flip}>
+          <div className="image">
+            <img src={pokemon.sprites.back} alt={pokemon.name} />
+          </div>
+          <div className="content">
+            <div className="header">{pokemon.name}</div>
+          </div>
+          <div className="extra content">
+            <span>
+              <i className="icon heartbeat red" />
+              {hp}
+            </span>
+          </div>
+        </div>
+      );
+    }
+  };
+
+  render() {
+    return <Card>{this.renderCardSide()}</Card>;
   }
 }
 
-export default PokemonCard
+export default PokemonCard;
